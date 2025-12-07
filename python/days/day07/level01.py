6259d18406d24ffe41b5145a8b486e55c8c4a259
Lars De Volder <36573562+larsdevolder@users.noreply.github.com>
2025-12-07 20:42:01 +0100

    python: day07 level1

--- python/days/day07/level01.py
+++ python/days/day07/level01.py
@@ -1,16 +1,23 @@
 import numpy as np
 
 diagram = []
-with open("inputs/day07/example.txt", encoding="utf8") as file:
+with open("inputs/day07/input.txt", encoding="utf8") as file:
     for line in file.readlines():
         diagram.append(list(line.strip()))
 diagram = np.array(diagram).T
 
+used_splitters = set()
+
 def beam(x, y):
+    global used_splitters
     n_split = 0
     while y < diagram.shape[1] and diagram[x][y] == '.':
         y += 1
-    assert diagram[x][y] == '^', "while loop gestopt, maar geen splitter"
+    if y == diagram.shape[1]:
+        return 0
+    if (x, y) in used_splitters:
+        return 0
+    used_splitters.add((x,y))
     n_split += 1
     n_split += beam(x-1, y)
     n_split += beam(x+1, y)
