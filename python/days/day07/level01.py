6345c2d856e11389505915595293d92e83382fca
Lars De Volder <36573562+larsdevolder@users.noreply.github.com>
2025-12-07 21:16:17 +0100

    python: dag07 level1 remove unnecessary logic and change some variable names

--- python/days/day07/level01.py
+++ python/days/day07/level01.py
@@ -7,15 +7,11 @@ with open("inputs/day07/input.txt", encoding="utf8") as file:
 diagram = np.array(diagram).T
 
 used_splitters = set()
-
 def beam(x, y):
-    global used_splitters
     n_split = 0
     while y < diagram.shape[1] and diagram[x][y] == '.':
         y += 1
-    if y == diagram.shape[1]:
-        return 0
-    if (x, y) in used_splitters:
+    if y == diagram.shape[1] or (x, y) in used_splitters:
         return 0
     used_splitters.add((x,y))
     n_split += 1
@@ -27,5 +23,5 @@ x = 0
 y = 0
 while diagram[x][y] != 'S':
     x += 1
-n_split = beam(x, 1)
-print(n_split)
+total_split = beam(x, 1)
+print(total_split)
