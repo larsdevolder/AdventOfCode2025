0693ea1da18b91a816602e97594e78d48a9d50a0
Lars De Volder <36573562+larsdevolder@users.noreply.github.com>
2025-12-08 15:29:44 +0100

    python: day07 level02 change variable name and improve logic to find starting location

--- python/days/day07/level02.py
+++ python/days/day07/level02.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-diagram = []
+diagram_list = []
 with open("inputs/day07/input.txt", encoding="utf8") as file:
     for line in file.readlines():
-        diagram.append(list(line.strip()))
-diagram = np.array(diagram).T
+        diagram_list.append(list(line.strip()))
+diagram = np.array(diagram_list).T
 
 known_splitters = {}
 def beam(x, y):
@@ -19,9 +19,6 @@ def beam(x, y):
         known_splitters[(x, y)] = n_timelines
     return known_splitters[(x,y)]
 
-x = 0
-y = 0
-while diagram[x][y] != "S":
-    x += 1
-total_timelines = beam(x, 1)
+x = np.where(diagram[:, 0] == 'S')[0][0]
+total_timelines = beam(x, 0)
 print(total_timelines)
