c27b4061bec092a705abe49818f82ddd223baf6a
Lars De Volder <36573562+larsdevolder@users.noreply.github.com>
2025-12-23 01:47:16 +0100

    python: day08 level02

---
+++ python/days/day08/level02.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy as sp
+
+junctionboxes_list = []
+with open("inputs/day08/input.txt", encoding="utf8") as file:
+    for line in file:
+        vector = list(map(int, line.split(",")))
+        junctionboxes_list.append(vector)
+
+junctionboxes = np.array(junctionboxes_list)
+
+distances = np.array(
+    sp.spatial.distance.cdist(junctionboxes, junctionboxes, "euclidean")
+)
+
+networks = [{tuple(junction)} for junction in junctionboxes]
+for idx in np.argsort(distances, axis=None)[::]:
+    row, col = np.unravel_index(idx, distances.shape)
+    junction1 = tuple(junctionboxes[row])
+    junction2 = tuple(junctionboxes[col])
+    if junction1 == junction2:
+        continue
+    same_network = False
+    network1 = set()
+    network2 = set()
+    for network in networks:
+        if junction1 in network and junction2 in network:
+            same_network = True
+            break
+        if junction1 in network:
+            network1 = network
+        if junction2 in network:
+            network2 = network
+    if same_network:
+        continue
+    network1.update(network2)
+    networks.remove(network2)
+    if len(networks) == 1:
+        print(junction1[0] * junction2[0])
