782e8c2dffdc52c3a8eac96cb3c2e6f1f9a111ec
Lars De Volder <36573562+larsdevolder@users.noreply.github.com>
2025-12-23 01:43:10 +0100

    python: day08 level01

--- python/days/day08/level01.py
+++ python/days/day08/level01.py
@@ -1,9 +1,8 @@
 import numpy as np
 import scipy as sp
-from collections import Counter
 
 junctionboxes_list = []
-with open("inputs/day08/example.txt", encoding="utf8") as file:
+with open("inputs/day08/input.txt", encoding="utf8") as file:
     for line in file:
         vector = list(map(int, line.split(',')))
         junctionboxes_list.append(vector)
@@ -12,20 +11,25 @@ junctionboxes = np.array(junctionboxes_list)
 
 distances = np.array(sp.spatial.distance.cdist(junctionboxes, junctionboxes, "euclidean"))
 
+pairs_used = set()
 networks = [{tuple(junction)} for junction in junctionboxes]
-CONNECTIONS = 9
+CONNECTIONS = 1000
+wires = 0
 for idx in np.argsort(distances, axis=None)[::]:
-    lengths = [len(network) - 1 for network in networks]
-    if sum(lengths) == CONNECTIONS:
+    if wires == CONNECTIONS:
         break
     row, col = np.unravel_index(idx, distances.shape)
     junction1 = tuple(junctionboxes[row])
     junction2 = tuple(junctionboxes[col])
     if junction1 == junction2:
         continue
+    if frozenset((junction1, junction2)) in pairs_used:
+        continue
+    pairs_used.add(frozenset((junction1, junction2)))
     same_network = False
     network1 = set()
     network2 = set()
+    wires += 1
     for network in networks:
         if junction1 in network and junction2 in network:
             same_network = True
@@ -39,4 +43,5 @@ for idx in np.argsort(distances, axis=None)[::]:
     network1.update(network2)
     networks.remove(network2)
 
-print(lengths)
+lengths = sorted([len(network) for network in networks], reverse=True)
+print(lengths[0] * lengths[1] * lengths[2])
